1629f98f6172261af119f7990d7ee64ec2bfb576
Kelvine-Palet <kelvine1.palet@gmail.com>
2024-09-04 13:39:55 +0200

    Add form

--- src/app/librairie/librairie.component.ts
+++ src/app/librairie/librairie.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ReadPipe } from '../utils/pipes/read/read.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-librairie',
   standalone: true,
   imports: [
-    ReadPipe
+    ReadPipe,
+    FormsModule
   ],
   templateUrl: './librairie.component.html',
   styleUrl: './librairie.component.css'
@@ -31,5 +33,26 @@ export class LibrairieComponent {
   changeIsRead(index: number) {
     this.livres[index].isRead = !this.livres[index].isRead;
   }
+
+  livre = {
+    title: '',
+    author: '',
+    isRead: false
+  }
+
+  submit(): void{
+
+    const livre1 = {
+      title: this.livre.title,
+      author: this.livre.author,
+      isRead: false
+    }
+    this.livres.push(livre1);
+    this.livre.title = "";
+    this.livre.author= "";
+
+
+  }
+  
 
 }
